5c0fce4c8387ffefc984f96747de2cb14bd0ef61
Anatolii Selivanov <anatolii.selivanov@orioninc.com>
2024-06-24 17:41:20 +0200

    wav file is embedded

--- main/main.cpp
+++ main/main.cpp
@@ -19,57 +19,23 @@
 #define TONE_LAST_TIME_MS 500
 #define BYTE_NUM_EVERY_TONE (TONE_LAST_TIME_MS * PDM_TX_FREQ_HZ / 1000)
 
-/* The frequency of tones: do, re, mi, fa, so, la, si, in Hz. */
-static const uint32_t tone[3][7] = {
-    {262, 294, 330, 349, 392, 440, 494},        // bass
-    {523, 587, 659, 698, 784, 880, 988},        // altoexam
-    {1046, 1175, 1318, 1397, 1568, 1760, 1976}, // treble
-};
-/* Numbered musical notation of 'twinkle twinkle little star' */
-static const uint8_t song[28] = {1, 1, 5, 5, 6, 6, 5,
-                                 4, 4, 3, 3, 2, 2, 1,
-                                 5, 5, 4, 4, 3, 3, 2,
-                                 5, 5, 4, 4, 3, 3, 2};
-/* Rhythm of 'twinkle twinkle little star', it's repeated in four sections */
-static const uint8_t rhythm[7] = {1, 1, 1, 1, 1, 1, 2};
-
-static const char *tone_name[3] = {"bass", "alto", "treble"};
+extern const uint8_t *example_wav_start asm("_binary_example_wav_start");
+// extern const uint8_t example_wav_end[] asm("_binary_example_wav_end");
+extern const size_t example_wav_size asm("example_wav_length");
 
 void i2s_pdm_tx_task(void *args)
 {
-    uint16_t *w_buf = (uint16_t *)calloc(1, BUFF_SIZE);
-    assert(w_buf);
-
-    uint8_t cnt = 0;         // The current index of the song
-    uint8_t tone_select = 0; // To selecting the tone level
 
     PDM_Player pdm_player(PDM_TX_CLK_IO, PDM_TX_DOUT_IO);
 
-    printf("Playing %s `twinkle twinkle little star`\n", tone_name[tone_select]);
     while (1)
     {
-        int tone_point = SINE_WAVE_LEN(tone[tone_select][song[cnt] - 1]);
-        /* Generate the tone buffer */
-        for (int i = 0; i < tone_point; i++)
-        {
-            w_buf[i] = (int16_t)((sin(2 * (float)i * CONST_PI / tone_point)) * WAVE_AMPLITUDE);
-        }
 
-        pdm_player.play(w_buf, BUFF_SIZE);
+        pdm_player.play((uint16_t *)example_wav_start, example_wav_size);
 
-        cnt++;
-        /* If finished playing, switch the tone level */
-        if (cnt == sizeof(song))
-        {
-            cnt = 0;
-            tone_select++;
-            tone_select %= 3;
-            printf("Playing %s `twinkle twinkle little star`\n", tone_name[tone_select]);
-        }
-        /* Gap between the tones */
         vTaskDelay(15);
     }
-    free(w_buf);
+
     vTaskDelete(NULL);
 }
 
