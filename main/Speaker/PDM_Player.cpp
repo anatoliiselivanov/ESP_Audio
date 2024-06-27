4d4702616ffde5ddc1a8717ea2eff1b6a021c67a
Anatolii Selivanov <anatolii.selivanov@orioninc.com>
2024-06-28 01:33:23 +0200

    pdm player is improved

--- main/Speaker/PDM_Player.cpp
+++ main/Speaker/PDM_Player.cpp
@@ -1,4 +1,5 @@
 #include "esp_err.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 
 #include "pdm_player.hpp"
@@ -12,10 +13,6 @@
 
 PDM_Player::PDM_Player(gpio_num_t clk_io, gpio_num_t data_io)
 {
-    i2s_chan_config_t tx_chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
-    tx_chan_cfg.auto_clear = true;
-    ESP_ERROR_CHECK(i2s_new_channel(&tx_chan_cfg, &m_tx_chan, NULL));
-
     m_pdm_tx_cfg = {
         .clk_cfg = I2S_PDM_TX_CLK_DEFAULT_CONFIG(PDM_TX_FREQ_HZ),
         .slot_cfg = I2S_PDM_TX_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
@@ -27,6 +24,28 @@ PDM_Player::PDM_Player(gpio_num_t clk_io, gpio_num_t data_io)
             },
         },
     };
+}
+
+void PDM_Player::play(uint8_t *buff, size_t size)
+{
+    WavFileReader wav_file(buff, size);
+    if (!wav_file.is_valid())
+    {
+        ESP_LOGE("PDM_Player", "Invalid wav file");
+        return;
+    }
+    init(wav_file.sample_rate(), wav_file.bits_per_sample(), wav_file.num_of_channels());
+    play(wav_file.data(), wav_file.data_size());
+}
+
+void PDM_Player::init(uint32_t sample_rate = PDM_TX_FREQ_HZ, uint32_t bit_width = I2S_DATA_BIT_WIDTH_16BIT, uint32_t channel_num = I2S_SLOT_MODE_MONO)
+{
+    i2s_chan_config_t tx_chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+    tx_chan_cfg.auto_clear = true;
+    ESP_ERROR_CHECK(i2s_new_channel(&tx_chan_cfg, &m_tx_chan, NULL));
+
+    m_pdm_tx_cfg.clk_cfg = I2S_PDM_TX_CLK_DEFAULT_CONFIG(sample_rate);
+    m_pdm_tx_cfg.slot_cfg = I2S_PDM_TX_SLOT_DEFAULT_CONFIG(i2s_data_bit_width_t(bit_width), i2s_slot_mode_t(channel_num));
 
     ESP_ERROR_CHECK(i2s_channel_init_pdm_tx_mode(m_tx_chan, &m_pdm_tx_cfg));
     ESP_ERROR_CHECK(i2s_channel_enable(m_tx_chan));
--- main/Speaker/PDM_Player.hpp
+++ main/Speaker/PDM_Player.hpp
@@ -1,3 +1,7 @@
+#pragma once
+
+#include "wav_file/wav_file_reader.hpp"
+
 #include "driver/i2s_pdm.h"
 #include "driver/gpio.h"
 
@@ -5,9 +9,13 @@ class PDM_Player
 {
 public:
     PDM_Player(gpio_num_t clk_io, gpio_num_t data_io);
-    void play(uint16_t *buff, size_t size);
+    void play(uint8_t *buff, size_t size);
+    void play(const char *file_path);
 
 private:
     i2s_pdm_tx_config_t m_pdm_tx_cfg;
     i2s_chan_handle_t m_tx_chan; // I2S tx channel handler
+
+    void init(uint32_t sample_rate, uint32_t bit_width, uint32_t channel_num);
+    void play(uint16_t *buff, size_t size);
 };
