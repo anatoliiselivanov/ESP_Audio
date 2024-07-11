443b506d9deebddcc488d0ee978d32e17d74671d
Anatolii Selivanov <anatolii.selivanov@orioninc.com>
2024-07-11 18:54:22 +0200

    Base virtual class for sound files is added

---
+++ main/sound_file.hpp
@@ -0,0 +1,17 @@
+#ifndef SOUND_FILE_HPP
+#define SOUND_FILE_HPP
+
+#include <stdint.h>
+#include <stddef.h>
+
+class SoundFile
+{
+public:
+    virtual ~SoundFile() = default;
+    virtual uint16_t num_of_channels() const = 0;
+    virtual uint32_t sample_rate() const = 0;
+    virtual uint32_t byte_rate() const = 0;
+    virtual uint16_t bits_per_sample() const = 0;
+    virtual size_t read(int16_t *samples, size_t number) = 0;
+};
+#endif // SOUND_FILE_HPP
