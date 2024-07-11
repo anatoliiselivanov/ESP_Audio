4be8999504dab86c7dfbafd4af2c933783946e8e
Anatolii Selivanov <anatolii.selivanov@orioninc.com>
2024-07-11 18:53:48 +0200

    mp3 files support is added

---
+++ main/mp3_file/mp3_file_reader.cpp
@@ -0,0 +1,90 @@
+#include "mp3_file_reader.hpp"
+
+const int BUFFER_SIZE = 1024;
+
+Mp3FileReader::Mp3FileReader(uint8_t *input_buf, size_t size)
+{
+    m_input_buf = input_buf;
+
+    // mp3 decoder state
+    mp3dec_init(&mp3d);
+}
+
+// void play_task(void *param)
+// {
+//     Output *output = new I2SOutput(I2S_NUM_0, i2s_speaker_pins);
+
+//     // setup for the mp3 decoded
+//     short *pcm = (short *)malloc(sizeof(short) * MINIMP3_MAX_SAMPLES_PER_FRAME);
+//     uint8_t *input_buf = (uint8_t *)malloc(BUFFER_SIZE);
+
+//     // mp3 decoder state
+//     mp3dec_t mp3d = {};
+//     mp3dec_init(&mp3d);
+//     mp3dec_frame_info_t info = {};
+
+//     // keep track of how much data we have buffered, need to read and decoded
+//     int to_read = BUFFER_SIZE;
+//     int buffered = 0;
+//     int decoded = 0;
+//     bool is_output_started = false;
+
+//     // this assumes that you have uploaded the mp3 file to the SPIFFS
+//     FILE *fp = fopen("/fs/test.mp3", "r");
+
+//     while (1)
+//     {
+
+//         // read in the data that is needed to top up the buffer
+//         size_t n = fread(input_buf + buffered, 1, to_read, fp);
+
+//         buffered += n;
+//         if (buffered == 0)
+//         {
+//             // we've reached the end of the file and processed all the buffered data
+//             output->stop();
+//             is_output_started = false;
+//             break;
+//         }
+
+//         // decode the next frame
+//         int samples = mp3dec_decode_frame(&mp3d, input_buf, buffered, pcm, &info);
+
+//         // we've processed this may bytes from teh buffered data
+//         buffered -= info.frame_bytes;
+
+//         // shift the remaining data to the front of the buffer
+//         memmove(input_buf, input_buf + info.frame_bytes, buffered);
+
+//         // we need to top up the buffer from the file
+//         to_read = info.frame_bytes;
+//         if (samples > 0)
+//         {
+//             // if we haven't started the output yet we can do it now as we now know the sample rate and number of channels
+//             if (!is_output_started)
+//             {
+//                 output->start(info.hz);
+//                 is_output_started = true;
+//             }
+
+//             // if we've decoded a frame of mono samples convert it to stereo by duplicating the left channel
+//             // we can do this in place as our samples buffer has enough space
+//             if (info.channels == 1)
+//             {
+//                 for (int i = samples - 1; i >= 0; i--)
+//                 {
+//                     pcm[i * 2] = pcm[i];
+//                     pcm[i * 2 - 1] = pcm[i];
+//                 }
+//             }
+
+//             // write the decoded samples to the I2S output
+//             output->write(pcm, samples);
+
+//             // keep track of how many samples we've decoded
+//             decoded += samples;
+//         }
+//     }
+
+//     fclose(fp);
+// }
---
+++ main/mp3_file/mp3_file_reader.hpp
@@ -0,0 +1,47 @@
+#ifndef MP3_FILE_READER_HPP
+#define MP3_FILE_READER_HPP
+
+#include "sound_file.hpp"
+
+#include "minimp3.h"
+
+class Mp3FileReader : public SoundFile
+{
+public:
+    Mp3FileReader(uint8_t *input_buf, size_t size);
+
+    uint16_t num_of_channels() const override
+    {
+        return info.channels;
+    }
+
+    uint32_t sample_rate() const override
+    {
+        return info.hz;
+    }
+
+    uint32_t byte_rate() const override
+    {
+        return info.bitrate_kbps * 1000 / 8;
+    }
+
+    uint16_t bits_per_sample() const override
+    {
+        return info.frame_bytes;
+    }
+
+    template <typename SampleT>
+    size_t read(SampleT *samples, size_t number)
+    {
+        size_t read_size = mp3dec_decode_frame(&mp3d, m_input_buf, number * sizeof(SampleT), samples, &info);
+        m_input_buf += read_size;
+        return read_size / sizeof(SampleT);
+    }
+
+private:
+    mp3dec_t mp3d = {};
+    mp3dec_frame_info_t info = {};
+    uint8_t *m_input_buf;
+};
+
+#endif // MP3_FILE_READER_HPP
